607d468b5db6620a20d17be3bdd65c9a7134e8e7
user <user@DESKTOP-UL7S2GB>
2024-12-22 22:38:49 +0200

    Register-css

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import './Register.css';
 
 export default function Register() {
   const [formData, setFormData] = useState({
