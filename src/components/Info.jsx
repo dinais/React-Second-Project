b0dc2bc3b6085783381ed514007316d3d496a89d
user <user@DESKTOP-UL7S2GB>
2025-01-12 19:49:21 +0200

    updated and organized info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,34 +1,86 @@
+// import { useUser } from "../contexts/useUser";
+// import {FaTimes } from "react-icons/fa";
+// import {PropTypes}from"prop-types"
+// export default function Info(props) {
+//   const { userData } = useUser();
+//   const storedUser = userData || JSON.parse(localStorage.getItem("currentUser"));
+// const{setShowProfileModal}=props;
+// const handleClose = () => {
+//     setShowProfileModal(false);
+//   };
+  
+//   return (
+//     <div className="sidebar-overlay">
+//     <div className="sidebar">
+//       <FaTimes className="close-btn" onClick={handleClose} />
+//       {storedUser ? (
+//         <ul>
+//           {Object.keys(storedUser).map((key, index) => (
+//             <li key={index} className="user-info">
+//               <strong>{key}:</strong> {JSON.stringify(storedUser[key], null, 2)}
+//             </li>
+//           ))}
+//         </ul>
+//       ) : (
+//         <p>No user data available</p>
+//       )}
+//     </div>
+//   </div>
+// );
+// }
+
+// Info.propTypes = {
+//     setShowProfileModal: PropTypes.func.isRequired
+//   };
 import { useUser } from "../contexts/useUser";
-import {FaTimes } from "react-icons/fa";
-import {PropTypes}from"prop-types"
+import { FaTimes } from "react-icons/fa";
+import PropTypes from "prop-types";
+
 export default function Info(props) {
   const { userData } = useUser();
   const storedUser = userData || JSON.parse(localStorage.getItem("currentUser"));
-const{setShowProfileModal}=props;
-const handleClose = () => {
+  const { setShowProfileModal } = props;
+  
+  const handleClose = () => {
     setShowProfileModal(false);
   };
-  
-  return (
-    <div className="sidebar-overlay">
-    <div className="sidebar">
-      <FaTimes className="close-btn" onClick={handleClose} />
-      {storedUser ? (
+
+  const renderValue = (value) => {
+    if (typeof value === "object" && value !== null) {
+      return (
         <ul>
-          {Object.keys(storedUser).map((key, index) => (
-            <li key={index} className="user-info">
-              <strong>{key}:</strong> {JSON.stringify(storedUser[key], null, 2)}
+          {Object.keys(value).map((subKey, subIndex) => (
+            <li key={subIndex}>
+              <span className="key">{subKey}:</span> {renderValue(value[subKey])}
             </li>
           ))}
         </ul>
-      ) : (
-        <p>No user data available</p>
-      )}
+      );
+    } else {
+      return <span>{value}</span>;
+    }
+  };
+
+  return (
+    <div className="sidebar-overlay">
+      <div className="sidebar">
+        <FaTimes className="close-btn" onClick={handleClose} />
+        {storedUser ? (
+          <ul>
+            {Object.keys(storedUser).map((key, index) => (
+              <li key={index} className="user-info">
+                <span className="key">{key}:</span> {renderValue(storedUser[key])}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No user data available</p>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 Info.propTypes = {
-    setShowProfileModal: PropTypes.func.isRequired
-  };
+  setShowProfileModal: PropTypes.func.isRequired
+};
